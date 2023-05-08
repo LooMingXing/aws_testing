c8e7659ff98315701c94037820c6fcaf1ca984a6
LooMingXing <111574090+LooMingXing@users.noreply.github.com>
2023-05-08 21:23:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ customhost = "employee.cwfh6qthrwn1.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "group1-employee"
+custombucket = "loomingxing-bucket"
 customregion = "us-east-1"
-
