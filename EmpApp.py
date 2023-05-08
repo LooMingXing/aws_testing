9d19035948fa94ac27841e86fb024676ffda4fab
LooMingXing <111574090+LooMingXing@users.noreply.github.com>
2023-05-08 21:01:05 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash
 from pymysql import connections
 import os
 import boto3
